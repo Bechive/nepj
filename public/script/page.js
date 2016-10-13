334238d6d7e51b2838790621a6d9003e22b15f3e
Benjamin Franzi <benjamin.franzi@outlook.com>
2016-10-14 00:19:26 +1100

    complexitating

--- public/script/page.js
+++ public/script/page.js
@@ -1,7 +1,30 @@
 window.onload = function() {
-  console.log("page loaded.")
+  render(decodeURI(window.location.hash))
 }
+
+$(window).on('hashchange', function(){
+		render(decodeURI(window.location.hash))
+	});
 
 function handle_click() {
   console.log("click event.")
+}
+
+
+function render(url) {
+  selector(url);
+  contentor(url);
+}
+
+//handles active tab
+function selector(div) {
+  document.getElementById('tab_home').className = ((div==''||div=='#Home')? 'active':'')
+  document.getElementById('tab_blog').className = ((div=='#Blog')? 'active':'')
+  document.getElementById('tab_projects').className = ((div=='#Projects')? 'active':'')
+}
+
+function contentor(div) {
+  document.getElementById('home').style.display = ((div==''||div=='#Home')? 'block':'none')
+  document.getElementById('blog').style.display = ((div=='#Blog')? 'block':'none')
+  document.getElementById('projects').style.display = ((div=='#Projects')? 'block':'none')
 }
